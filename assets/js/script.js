4d86f1f4c3a5148300062822b243b51baa727ce8
Gianncarlo Ciampaglia <giank7@gmail.com>
2022-09-23 21:25:31 +0000

    Add instructions and styles and script to hide the instructions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@
 const startingBtn = document.getElementById('front-btn');
 const startingImg = document.getElementById('front-img');
 const startingText = document.getElementById('text-front');
-
+const readyOne = document.getElementById('readyone');
+const readyTwo = document.getElementById('readytwo');
+const readyBtn = document.getElementById('ready-btn');
 /**
  * Hide the starting page
  */
@@ -11,6 +13,20 @@ function startInstructions(){
     startingImg.style.display = "none";
     startingText.style.display = "none";
     startingBtn.style.display = "none";
+    readyBtn.style.display = "inline";
+    readyOne.style.display = "inline";
+    readyTwo.style.display = "inline";
 }
-
+// Initial button
 startingBtn.addEventListener('click',startInstructions)
+// Ready button
+readyBtn.addEventListener('click', startGame)
+
+/**
+ * Hide the instructions and start the game
+ */
+function startGame(){
+    readyOne.style.display = "none";
+    readyTwo.style.display = "none";
+    readyBtn.style.display = "none";
+}
