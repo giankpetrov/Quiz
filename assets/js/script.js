27262e572011f15991f2ee2bf645b339cc595ff5
Gianncarlo Ciampaglia <giank7@gmail.com>
2022-09-25 03:21:15 +0000

    fix button area of action for scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 
 
-const startingBtn = document.getElementById('front-btn');
+const startingBtn = document.getElementById('front-btn-area');
 const startingImg = document.getElementById('front-img');
 const startingText = document.getElementById('text-front');
 const readyOne = document.getElementById('readyone');
 const playBtn = document.getElementById('play-btn');
+const playBtnArea = document.getElementById('play-btn-area');
 /**
  * Hide the starting page
  */
@@ -18,7 +19,7 @@ function startInstructions(){
 // Initial button
 startingBtn.addEventListener('click',startInstructions)
 // Play button
-playBtn.addEventListener('click', startGame)
+playBtnArea.addEventListener('click', startGame)
 
 /**
  * Hide the instructions and start the game
