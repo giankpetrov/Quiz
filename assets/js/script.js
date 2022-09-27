6bb32712c5a986524761f99213dc02cd8a88405b
Gianncarlo Ciampaglia <giank7@gmail.com>
2022-09-27 23:37:14 +0000

    Add script for random question and answers styles responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,41 @@ const readyOne = document.getElementById('readyone');
 const playBtn = document.getElementById('play-btn');
 const playBtnArea = document.getElementById('play-btn-area');
 const gameArea = document.getElementById('game-area');
-const question = document.getElementById('question');
+const questionArea = document.getElementById('question-area');
+const questionElement = document.getElementById('question')
+const answerOptions = document.getElementById('answer-box')
 const choiceOne = document.getElementById('choiceOne');
 const choiceTwo = document.getElementById('choiceTwo');
 const choiceThree = document.getElementById('choiceThree');
 const choiceFour = document.getElementById('choiceFour');
+
+
+let shuffledQuestions
+let currentQuestionIndex
+
+
+const questions = [
+    {
+    question: 'Which was the last nation to win the FIFA World Cup as the host?',
+    answers: [
+        'France',
+        'Brazil',
+        'Germany',
+        'Italy'
+        ],
+    correctAnswer: 'France'
+    },
+    {
+    question: 'Who is the highest goal scorer in the history of the FIFA World Cup?',
+    answers: [
+        'Gerd Muller',
+        'Pele',
+        'Miroslav Klose',
+        'Ronaldo Nazario'
+        ],
+    correctAnswer: 'Miroslav Klose'        
+    }
+]
 
 /**
  * Hide the starting page
@@ -34,10 +64,26 @@ playBtnArea.addEventListener('click', startGame)
 function startGame(){
     readyOne.style.display = "none";
     playBtn.style.display = "none";
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     gameArea.style.display = "inline";
-    question.style.display = "inline";
+    questionArea.style.display = "inline";
     choiceOne.style.display = "inline";
     choiceTwo.style.display = "inline";
     choiceThree.style.display = "inline";
     choiceFour.style.display = "inline";
+    setNextQuestion();
+
+}
+
+function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question){
+    questionElement.innerText = question.question
+    choiceOne.innerText = question.answers[0];
+    choiceTwo.innerText = question.answers[1];
+    choiceThree.innerText = question.answers[2];
+    choiceFour.innerText = question.answers[3];
 }
