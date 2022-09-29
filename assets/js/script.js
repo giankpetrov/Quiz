be8a5828bb7d7b3d5044ec1d71f2f7ce3658c0cd
Gianncarlo Ciampaglia <giank7@gmail.com>
2022-09-29 00:54:27 +0000

    Add styles when selecting answer and scrypt to create next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 
-
 const startingBtn = document.getElementById('front-btn-area');
 const startingImg = document.getElementById('front-img');
 const startingText = document.getElementById('text-front');
@@ -7,39 +6,47 @@ const readyOne = document.getElementById('readyone');
 const playBtn = document.getElementById('play-btn');
 const playBtnArea = document.getElementById('play-btn-area');
 const gameArea = document.getElementById('game-area');
-const questionArea = document.getElementById('question-area');
-const questionElement = document.getElementById('question')
+const questionArea = document.getElementById('question-area'); // question area class
+const questionElement = document.getElementById('question') // question text
 const answerOptions = document.getElementById('answer-box')
 const choiceOne = document.getElementById('choiceOne');
 const choiceTwo = document.getElementById('choiceTwo');
 const choiceThree = document.getElementById('choiceThree');
 const choiceFour = document.getElementById('choiceFour');
 
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 
-let shuffledQuestions
-let currentQuestionIndex
+let currentQuestion = {}
+let acceptingAnswers = true
+let questionCounter = 0
+let availableQuestions = []
+let score= 0
 
 
 const questions = [
     {
     question: 'Which was the last nation to win the FIFA World Cup as the host?',
-    answers: [
-        'France',
-        'Brazil',
-        'Germany',
-        'Italy'
-        ],
-    correctAnswer: 'France'
+    choice1: 'France',
+    choice2: 'Brazil',
+    choice3: 'Germany',
+    choice4: 'Italy',
+    answer: 1,
     },
     {
     question: 'Who is the highest goal scorer in the history of the FIFA World Cup?',
-    answers: [
-        'Gerd Muller',
-        'Pele',
-        'Miroslav Klose',
-        'Ronaldo Nazario'
-        ],
-    correctAnswer: 'Miroslav Klose'        
+    choice1: 'Gerd Muller',
+    choice2: 'Pele',
+    choice3: 'Miroslav Klose',
+    choice4: 'Ronaldo Nazario',
+    answer: 3,   
+    },
+    {
+    question: 'Which country has won the most FIFA World Cups?',
+    choice1: 'Germany',
+    choice2: 'Brazil',
+    choice3: 'Italy',
+    choice4: 'England',
+    answer: 2,   
     }
 ]
 
@@ -58,32 +65,76 @@ startingBtn.addEventListener('click',startInstructions)
 // Play button
 playBtnArea.addEventListener('click', startGame)
 
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 10
+
 /**
  * Hide the instructions and start the game
  */
 function startGame(){
     readyOne.style.display = "none";
     playBtn.style.display = "none";
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
     gameArea.style.display = "inline";
     questionArea.style.display = "inline";
     choiceOne.style.display = "inline";
     choiceTwo.style.display = "inline";
     choiceThree.style.display = "inline";
     choiceFour.style.display = "inline";
-    setNextQuestion();
-
+    questionCounter = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
 }
 
-function setNextQuestion(){
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecenScore', score)
+
+        return window.location.assign('/end.html')
+    }
+    
+    questionCounter++
+    
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    questionElement.innerText = currentQuestion.question
+    //This will asing the choices text from current question
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice'+ number]
+    })
+    // This will delete the current question from the question array
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+// Add event listener to each available choice
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if(classToApply === 'correct'){
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.classList.remove(classToApply)
+            getNewQuestion()
+
+        }, 2000)
+    })
+})
+
+let incrementScore = num => {
+    score +=num;
 }
 
-function showQuestion(question){
-    questionElement.innerText = question.question
-    choiceOne.innerText = question.answers[0];
-    choiceTwo.innerText = question.answers[1];
-    choiceThree.innerText = question.answers[2];
-    choiceFour.innerText = question.answers[3];
-}
+
