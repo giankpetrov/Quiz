bb6ee1f811cff6d41bba18fa46acce09159bbb20
Gianncarlo Ciampaglia <giank7@gmail.com>
2022-10-03 02:14:38 +0000

    ADD extra info text and next question button HTML CSS and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,15 @@ const gameArea = document.getElementById('game-area')
 const questionArea = document.getElementById('question-area') // question area class
 const questionElement = document.getElementById('question') // question text
 const answerOptions = document.getElementById('answer-box')
+const choices = Array.from(document.querySelectorAll('.choice-text')) // Array of choices in HTML
+const extraInfo = document.getElementById('extraInfo') // Extra Info Area
+const nextQuestionBtn = document.getElementById('next-question-btn')
+const infoText = document.getElementById('info-text'); 
 const endArea = document.getElementById('end-area')
 const username = document.getElementById('username')
 const saveScore = document.getElementById('saveScoreBtn')
 const finalScore = document.getElementById('finalScore')
 const playAgain = document.getElementById('playAgain')
-const choices = Array.from(document.querySelectorAll('.choice-text')) // Array of choices in HTML
 const scoreText = document.getElementById('score')
 const highScoresArea = document.getElementById('highScoresArea')
 const highScoresList = document.getElementById('highScoresList')
@@ -38,6 +41,7 @@ const questions = [
     choice3: 'Germany',
     choice4: 'Italy',
     answer: 1,
+    extra: 'France won the World Cup as the host in 1998'
     },
     {
     question: 'Who is the highest goal scorer in the history of the FIFA World Cup?',
@@ -45,7 +49,8 @@ const questions = [
     choice2: 'Pele',
     choice3: 'Miroslav Klose',
     choice4: 'Ronaldo Nazario',
-    answer: 3,   
+    answer: 3,
+    extra: 'Miroslav Klose scored 16 goals between 2002 and 2014'   
     },
     {
     question: 'Which country has won the most FIFA World Cups?',
@@ -53,7 +58,8 @@ const questions = [
     choice2: 'Brazil',
     choice3: 'Italy',
     choice4: 'England',
-    answer: 2,   
+    answer: 2,
+    extra: 'Brazil have won 5 tournaments. 1958, 1962, 1970, 1994 and 2002'    
     },
     {
     question: 'Which country have never qualified for the FIFA World Cup?',
@@ -62,6 +68,7 @@ const questions = [
     choice3: 'Venezuela',
     choice4: 'Haiti',
     answer: 3,   
+    extra: 'Cuba went in 1938, Haiti in 1974 and North Korea 1966 and 2010'
     },
     {
     question: 'Which country has the most World Cup losses?',
@@ -70,6 +77,7 @@ const questions = [
     choice3: 'Mexico',
     choice4: 'England',
     answer: 3,   
+    extra: 'Mexico has lost 27 out of 57 games in the World Cup'
     },
     {
     question: 'How old was the oldest player to ever play in the FIFA World Cup?',
@@ -78,6 +86,7 @@ const questions = [
     choice3: '42',
     choice4: '45',
     answer: 4,   
+    extra: 'Essam El-Hadary from Egypt play in 2018 with 45 years and 161 days old'
     },
     {
     question: 'How old was the youngest player to ever play in the FIFA World Cup?',
@@ -85,7 +94,8 @@ const questions = [
     choice2: '19',
     choice3: '18',
     choice4: '16',
-    answer: 1,   
+    answer: 1,
+    extra: 'Norman Whiteside from Northern Ireland debuted with 17 years and 40 days old in 1982'   
     },
     {
     question: 'How fast was the fastest ever FIFA World Cup goal?',
@@ -94,6 +104,7 @@ const questions = [
     choice3: '8.2 seconds',
     choice4: '10.8 seconds',
     answer: 4,   
+    extra: 'From Turkey, Hakan Sukur scored against South Korea in 2002'
     },
     {
     question: 'Which country has the most World Cup wins?',
@@ -102,6 +113,7 @@ const questions = [
     choice3: 'Germany',
     choice4: 'Argentina',
     answer: 1,   
+    extra: 'Brazil have won 73 out of 109 games played in the World Cup'
     },
     {
     question: 'Which country have been eliminated in the first round the most times?',
@@ -110,6 +122,7 @@ const questions = [
     choice3: 'Scotland',
     choice4: 'Mexico',
     answer: 3,   
+    extra: 'Scotland have been eliminated 8 out of 8 times in the first round'
     },
     {
     question: 'Where was the very first World Cup held?',
@@ -118,6 +131,7 @@ const questions = [
     choice3: 'Uruguay',
     choice4: 'Italy',
     answer: 3,   
+    extra: 'In 1930, Uruguay held the first World Cup'
     },
     {
     question: 'How often is the FIFA World Cup?',
@@ -126,6 +140,7 @@ const questions = [
     choice3: '4 years',
     choice4: '2 years',
     answer: 3,   
+    extra: '4 years gap allows time for the qualification fixtures'
     },
     {
     question: 'When was the first World Cup held?',
@@ -134,6 +149,7 @@ const questions = [
     choice3: '1950',
     choice4: '1940',
     answer: 1,   
+    extra: 'Uruguay held the first World Cup in 1930'
     },
     {
     question: 'Which country hosted the World Cup in 2018?',
@@ -142,6 +158,7 @@ const questions = [
     choice3: 'France',
     choice4: 'Croatia',
     answer: 2,   
+    extra: 'Russia was selected to host the 2018 World Cup back in 2010'
     },
     {
     question: 'Which country will host the World Cup in 2022?',
@@ -150,6 +167,7 @@ const questions = [
     choice3: 'Qatar',
     choice4: 'Mexico',
     answer: 3,   
+    extra: 'Qatar was selected to host the 2022 World Cup back in 2010'
     },
     {
     question: 'How many teams participate in the tournament phase?',
@@ -158,6 +176,7 @@ const questions = [
     choice3: '28 teams',
     choice4: '32 teams',
     answer: 4,   
+    extra: 'The tournament was expanded to 32 teams in 1998'
     },
     {
     question: 'Which is the only team that played in every tournament?',
@@ -166,14 +185,16 @@ const questions = [
     choice3: 'Italy',
     choice4: 'Argentina',
     answer: 1,   
+    extra: 'Brazil has participated in all 21 World Cups'
     },
     {
-    question: 'Which team have the nickname "Azurri"?',
+    question: 'Which team have the nickname "Azzurri"?',
     choice1: 'Italy',
     choice2: 'France',
     choice3: 'Uruguay',
     choice4: 'Argentina',
     answer: 1,   
+    extra: 'Traditional Savoy Blue, Italy is know as the "Azzurri'
     },
     {
     question: 'Which team have the nickname "Three Lions"?',
@@ -182,15 +203,17 @@ const questions = [
     choice3: 'England',
     choice4: 'Sri Lanka',
     answer: 3,   
+    extra: 'England use Three Lions in their uniform since 1872'
     },
     {
-    question: 'Which team have the nickname "The Red Fury?',
+    question: 'Which team have the nickname "The Red Fury"?',
     choice1: 'Chile',
     choice2: 'Portugal',
     choice3: 'China',
     choice4: 'Spain',
     answer: 4,   
-    },  
+    extra: 'Spain is "The Red Fury" for the strong and physical way of playing'
+    },
 ]
 
 /**
@@ -200,6 +223,7 @@ function startInstructions(){
     startingImg.style.display = "none";
     startingText.style.display = "none";
     startingBtn.style.display = "none";
+    leaderBoardBtn.style.display = "none";
     playBtn.style.display = "inline";
     readyOne.style.display = "inline";
 }
@@ -220,7 +244,7 @@ playBtnArea.addEventListener('click', startGame)
 
 
 const SCORE_POINTS = 100
-const MAX_QUESTIONS = 1
+const MAX_QUESTIONS = 2
 
 /**
  * Hide the instructions and start the game
@@ -229,6 +253,7 @@ function startGame(){
     readyOne.style.display = "none";
     playBtn.style.display = "none";
     gameArea.style.display = "inline";
+    extraInfo.style.display = "none";
     scoreText.innerText = 0;
     questionCounter = 0
     score = 0
@@ -248,11 +273,12 @@ getNewQuestion = () => {
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
     questionElement.innerText = currentQuestion.question
-    //This will asing the choices text from current question
+    //This will asign the choices text from current question
     choices.forEach(choice => {
         const number = choice.dataset['number']
         choice.innerText = currentQuestion['choice'+ number]
     })
+    infoText.innerText = currentQuestion['extra']
     // This will delete the current question from the question array
     availableQuestions.splice(questionsIndex, 1)
 
@@ -277,13 +303,20 @@ choices.forEach(choice => {
                 incrementScore(SCORE_POINTS)
                 scoreText.classList.add('scored')
             }
-            
+            setTimeout(() =>{
+                extraInfo.style.display = "inline"
+            },1000 )
             //Later will remove class and get new question
             setTimeout(() => {
-                selectedChoice.classList.remove(classToApply)
-                scoreText.classList.remove('scored')
-                getNewQuestion()
-            }, 1000)
+                nextQuestionBtn.style.display = "inline"
+                nextQuestionPlease = e => {
+                    selectedChoice.classList.remove(classToApply)
+                    scoreText.classList.remove('scored')
+                    extraInfo.style.display = "none"
+                    nextQuestionBtn.style.display = "none"
+                    getNewQuestion()
+                }
+            }, 5000)
         }, 1500)
     })
 })
